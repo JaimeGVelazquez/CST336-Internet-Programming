08970850fbbb49b9750d0a2e6705315e9e2ef378
Jaime Velazquez <jaimegvelazquez@gmail.com>
2017-08-25 20:20:48 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+            <h1> Welcome to Miguel's CST336 website</h1>
+    </body>
+</html>
