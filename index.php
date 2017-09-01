597d7d06e604a017ac065cb2be10e52f2847dc90
Jaime Velazquez <jaimegvelazquez@gmail.com>
2017-09-01 19:21:59 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<!--
+First Website
+and comment
+in html
+(comments can span multiple lines)
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Jaime Velazquez: Personal Website</title>
+       <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <p>Welcome to Jaime's website!</p>
+    </body>
+        
+        
+</html>
